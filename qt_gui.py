3eb94abf99cea65657c88aedf824de2f659a4ff5
stihl1210 <stihl1210@gmail.com>
2017-11-04 18:26:22 +0100

    code cleaning and pep 8 validation

--- qt_gui.py
+++ qt_gui.py
@@ -1,12 +1,14 @@
 #! /usr/bin/python
 
 from __future__ import print_function
+
+from __future__ import absolute_import
 import sys
 import os.path
 import vlc
 from PyQt4 import QtGui, QtCore
-from VideoEvents import model, controller
-import VideoEvents.model as mdl
+from .VideoEvents import controller
+from .VideoEvents import model as mdl
 import random
 from shutil import copyfile
 import json
@@ -15,38 +17,38 @@ import json
 class VideoEvtWidget(QtGui.QWidget):
     def __init__(self, videoEventModel):
         QtGui.QWidget.__init__(self)
-        self.label = QtGui.QLabel( videoEventModel.get_description() )
+        self.label = QtGui.QLabel(videoEventModel.get_description())
         layout = QtGui.QHBoxLayout()
-        layout.addWidget(self.label);
+        layout.addWidget(self.label)
         self.setLayout(layout)
 
     def remove_model(self):
         print('controller removing model from dict')
 
 
-def showVideoAlert(layout, info="Video Error", defaultResponse="There is no video resource to analyze"):
+def showVideoAlert(layout, info="Video Error",
+                   defaultResponse="There is no video resource to analyze"):
     QtGui.QMessageBox.about(layout, info, defaultResponse)
 
 
 class VideoEventsMenu(QtGui.QWidget):
-
     def __init__(self, eventsController):
         QtGui.QWidget.__init__(self)
         self.controller = eventsController
 
         self.start = QtGui.QPushButton("Start")
-        self.stop = QtGui.QPushButton("Stop");
+        self.stop = QtGui.QPushButton("Stop")
         self.add = QtGui.QPushButton("Add")
         self.resource = QtGui.QPushButton("Resource")
         self.serialize = QtGui.QPushButton("Serialize")
 
-        self.startLab = QtGui.QLabel("Start: ");
+        self.startLab = QtGui.QLabel("Start: ")
 
         self.startLab.setMaximumHeight(20)
-        self.stopLab = QtGui.QLabel("Stop: ");
-        self.posLab = QtGui.QLabel("Pos: ");
+        self.stopLab = QtGui.QLabel("Stop: ")
+        self.posLab = QtGui.QLabel("Pos: ")
         self.posLab.setMaximumHeight(20)
-        self.resLab = QtGui.QLabel("Res: ");
+        self.resLab = QtGui.QLabel("Res: ")
 
         self.eventLayout = QtGui.QGridLayout()
         self.eventLayout.setAlignment(QtCore.Qt.AlignTop)
@@ -63,7 +65,6 @@ class VideoEventsMenu(QtGui.QWidget):
         self.eventLayout.addWidget(self.stopLab, 2, 1)
         self.eventLayout.addWidget(self.posLab, 2, 2)
         self.eventLayout.addWidget(self.resLab, 3, 0)
-
 
         self.layout.addLayout(self.eventLayout)
         self.setLayout(self.layout)
@@ -100,40 +101,37 @@ class VideoEventsMenu(QtGui.QWidget):
             os.makedirs(self.outdir)
 
     def serializeAction(self):
-        if(not self.player_vlc ):
+        if not self.player_vlc:
             showVideoAlert(self)
             return
 
-        if(len(self.models_list)==0):
+        if len(self.models_list) == 0:
             return
-        filename = QtGui.QFileDialog.getSaveFileName(self, "Save File", os.path.expanduser(os.getcwd()))
+        filename = QtGui.QFileDialog.\
+            getSaveFileName(self, "Save File", os.path.expanduser(os.getcwd()))
 
-        if (len(filename) == 0):
+        if len(filename) == 0:
             return
 
-
-        print ('outdir, fileout name ', self.outdir, self.outname)
+        print('outdir, fileout name ', self.outdir, self.outname)
 
         serialized_dict = dict()
         serialized_dict['file_name'] = str(self.video_name)
         serialized_dict['events'] = [ob.__dict__ for ob in self.models_list]
 
-
         with open(filename, 'w') as outfile:
 
             strx = json.dumps(serialized_dict, sort_keys=True, indent=4)
-            # strx = json.dumps( [ob.__dict__ for ob in self.models_list] , sort_keys=True, indent=4)
             outfile.write(strx)
 
-        if(os.path.exists(self.outdir+'/resources/')==0):
-            os.mkdir(self.outdir+'resources/')
+        if os.path.exists(self.outdir + '/resources/') == 0:
+            os.mkdir(self.outdir + 'resources/')
 
-        print('resources ' , self.resources)
+        print('resources ', self.resources)
         for it, (name, dir) in enumerate(self.resources):
-            print ('enumerate copy ', name, dir)
-            print ('saving to ', self.outdir+'/resources/'+name)
-            copyfile(dir, self.outdir+'/resources/'+name)
-
+            print('enumerate copy ', name, dir)
+            print('saving to ', self.outdir + '/resources/' + name)
+            copyfile(dir, self.outdir + '/resources/' + name)
 
     def pass_player_ref(self, playerRef, facadePlayer):
         print('menu player - passed ref')
@@ -142,26 +140,24 @@ class VideoEventsMenu(QtGui.QWidget):
 
     def set_file_outputs_names(self, filename):
         self.video_name = filename
-        self.openedFileDir = filename.split('.')[-2];
-        print('opened file dir ' , self.openedFileDir)
+        self.openedFileDir = filename.split('.')[-2]
+        print('opened file dir ', self.openedFileDir)
 
     def getPos(self):
-        if(not self.player_vlc ):
+        if not self.player_vlc:
             showVideoAlert(self)
             return
-        if(self.playerFacadeRef):
+        if self.playerFacadeRef:
             self.posx, self.posy = self.playerFacadeRef.lastPos
-            print('menu pos ' , self.posx, self.posy);
-            self.posLab.setText("Pos: " + str( (self.posx, self.posy) ))
-
+            print('menu pos ', self.posx, self.posy)
+            self.posLab.setText("Pos: " + str((self.posx, self.posy)))
 
     def setstop(self):
         print('set stop')
-        if(not self.player_vlc ):
+        if not self.player_vlc:
             showVideoAlert(self)
             return
-        print (self.player_vlc.get_time())
-        if(self.player_vlc.get_time() == -1):
+        if self.player_vlc.get_time() == -1:
             self.stoptime = 0
         else:
             self.stoptime = self.player_vlc.get_time()
@@ -169,67 +165,67 @@ class VideoEventsMenu(QtGui.QWidget):
 
     def setstart(self):
         print('set start')
-        if(not self.player_vlc ):
+        if not self.player_vlc:
             showVideoAlert(self)
-            return;
-        print (self.player_vlc.get_time())
-        if(self.player_vlc.get_time() == -1):
-            self.starttime = 0;
+            return
+        if self.player_vlc.get_time() == -1:
+            self.starttime = 0
         else:
             self.starttime = self.player_vlc.get_time()
 
         self.startLab.setText("Start: " + str(self.starttime))
 
     def setres(self):
-        if(not self.player_vlc ):
+        if not self.player_vlc:
             showVideoAlert(self)
             return
         print('set res')
         dir = os.getcwd()
-        fnamedir = QtGui.QFileDialog.getOpenFileName(self, 'Open file', dir)
+        fnamedir = QtGui.QFileDialog.\
+            getOpenFileName(self, 'Open file', dir)
 
-        if(len(fnamedir)==0):
+        if len(fnamedir) == 0:
             return
 
         name = fnamedir.split('/')[-1]
-        print ('res ' , name)
-        self.resources.append((name,fnamedir) )
+        print('res ', name)
+        self.resources.append((name, fnamedir))
 
-        dirout = os.getcwd()+"/data/"
+        dirout = os.getcwd() + "/data/"
 
-        if (os.path.exists(dirout) == 0):
+        if os.path.exists(dirout) == 0:
             os.mkdir(dirout)
 
-        diroutresources = os.getcwd()+"/data/"+self.openedFileDir
+        diroutresources = os.getcwd() + "/data/" + self.openedFileDir
         print('dirout resources ', diroutresources)
 
-        if (os.path.exists(diroutresources) == 0):
+        if os.path.exists(diroutresources) == 0:
             os.mkdir(diroutresources)
 
-        copyfile(fnamedir, diroutresources+"/"+name)
+        copyfile(fnamedir, diroutresources + "/" + name)
         self.resname = name
         self.resLab.setText("Res: " + str(self.resname))
 
-
     def addVideoEvent(self):
-        if(not self.resname or  not self.starttime or  not self.stoptime):
-            print('set start stop and res');
+        if not self.resname or not self.starttime or not self.stoptime:
+            print('set start stop and res')
             return
-        if(not self.posx and not self.posy):
+        if not self.posx and not self.posy:
             return
 
-        id = random.randint(0,2**32-1)
+        id = random.randint(0, 2 ** 32 - 1)
 
-        fileType = str(self.resname).split('.')[-1];
-        type = 'image'
-        if(fileType == 'jpg' or fileType == 'gif' or fileType == 'jpeg' or fileType == 'png'):
+        fileType = str(self.resname).split('.')[-1]
+        if fileType == 'jpg' or fileType == 'gif' or fileType == 'jpeg' or fileType == 'png':
             type = 'image'
-        elif(fileType=='mp3' or fileType=='wav' or fileType == 'ogg'):
+        elif fileType == 'mp3' or fileType == 'wav' or fileType == 'ogg':
             type = 'audio'
         else:
             type = 'text'
 
-        model = mdl.VideoEventModel(id,self.starttime, self.stoptime, self.posx, self.posy, type, str(self.resname))
+        model = mdl.VideoEventModel(id, self.starttime,
+                                    self.stoptime, self.posx,
+                                    self.posy, type, str(self.resname))
         self.controller.addModel(model)
         self.models_list.append(model)
 
@@ -246,28 +242,25 @@ class VideoEventsMenu(QtGui.QWidget):
 
         removeBtn.clicked.connect(lambda state, x=id: self.button_pushed(x, widg))
 
-
         self.layout.addWidget(widg)
 
         self.starttime = self.stoptime = self.resname = False
         self.cleanLabels()
 
     def button_pushed(self, num, boxref):
-        print ('Pushed button {}'.format(num))
+        print('Pushed button {}'.format(num))
         self.controller.removeModel(num)
         self.layout.removeWidget(boxref)
         boxref.deleteLater()
 
     def cleanLabels(self):
-        self.startLab.setText("Start: ");
-        self.stopLab.setText("Stop: ");
-        self.posLab.setText("Pos: ");
-        self.resLab.setText("Res: ");
-
+        self.startLab.setText("Start: ")
+        self.stopLab.setText("Stop: ")
+        self.posLab.setText("Pos: ")
+        self.resLab.setText("Res: ")
 
 
 class VideoFrame(QtGui.QWidget):
-
     def __init__(self):
         QtGui.QWidget.__init__(self)
         self.click_positions = []
@@ -278,7 +271,7 @@ class VideoFrame(QtGui.QWidget):
         imageSize = QtCore.QSize(500, 500)
         self.image = QtGui.QImage(imageSize, QtGui.QImage.Format_RGB32)
         self.text = ""
-        self.lastPos = (0,0)
+        self.lastPos = (0, 0)
         self.player_vlc = None
 
     def pass_player_ref(self, playerRef):
@@ -288,23 +281,26 @@ class VideoFrame(QtGui.QWidget):
         self.video_side_menu = video_side
 
     def mousePressEvent(self, QMouseEvent):
-        print ('mouse pressed')
-        if(not self.player_vlc ):
-            return;
-        print (self.player_vlc.video_get_cursor())
-        print (self.player_vlc.get_time())
+        print('mouse pressed')
+        if not self.player_vlc:
+            return
+        print(self.player_vlc.video_get_cursor())
+        print(self.player_vlc.get_time())
         self.lastPos = self.player_vlc.video_get_cursor()
-        print ('last pos ', self.lastPos)
+        print('last pos ', self.lastPos)
 
         self.pixelSelect(self.player_vlc.video_get_cursor())
         self.video_side_menu.getPos()
 
     def pixelSelect(self, eventpos):
-        print ('pixel select')
+        print('pixel select')
         painter = QtGui.QPainter(self.image)
         painter.setPen(QtGui.QPen(QtCore.Qt.red, 55,
-                                  QtCore.Qt.SolidLine, QtCore.Qt.RoundCap, QtCore.Qt.RoundJoin))
-        painter.drawLine(QtCore.QPoint(22,22), QtCore.QPoint(100,100))
+                                  QtCore.Qt.SolidLine,
+                                  QtCore.Qt.RoundCap,
+                                  QtCore.Qt.RoundJoin))
+
+        painter.drawLine(QtCore.QPoint(22, 22), QtCore.QPoint(100, 100))
 
         self.update()
 
@@ -312,21 +308,19 @@ class VideoFrame(QtGui.QWidget):
 class Player(QtGui.QMainWindow):
     """A simple Media Player using VLC and Qt
     """
+
     def __init__(self, master=None):
         QtGui.QMainWindow.__init__(self, master)
         self.setWindowTitle("Media Player")
 
-        # creating a basic vlc instance
         self.instance = vlc.Instance("--no-xlib")
-        # creating an empty vlc media player
         self.mediaplayer = self.instance.media_player_new()
 
         self.createUI()
         self.isPaused = False
 
     def clicked(self, clicked):
-        print ('clicked ', str(clicked))
-
+        print('clicked ', str(clicked))
 
     def createUI(self):
         """Set up the user interface, signals & slots
@@ -335,7 +329,7 @@ class Player(QtGui.QMainWindow):
         self.setCentralWidget(self.widget)
 
         # In this widget, the video will be drawn
-        if sys.platform == "darwin": # for MacOS
+        if sys.platform == "darwin":  # for MacOS
             self.videoframe = QtGui.QMacCocoaViewContainer(0)
         else:
 
@@ -344,8 +338,8 @@ class Player(QtGui.QMainWindow):
         #                  self.clicked)
 
         self.palette = self.videoframe.palette()
-        self.palette.setColor (QtGui.QPalette.Window,
-                               QtGui.QColor(0,0,0))
+        self.palette.setColor(QtGui.QPalette.Window,
+                              QtGui.QColor(0, 0, 0))
         self.videoframe.setPalette(self.palette)
         self.videoframe.setAutoFillBackground(True)
 
@@ -380,24 +374,21 @@ class Player(QtGui.QMainWindow):
         self.vboxlayout.addWidget(self.videoframe)
         self.vboxlayout.addWidget(self.positionslider)
         self.vboxlayout.addLayout(self.hbuttonbox)
-        
+
         self.controller = controller.VideoEventsController()
 
         self.sideeventmenu = VideoEventsMenu(self.controller)
 
-        self.hboxlayout = QtGui.QHBoxLayout(self);
+        self.hboxlayout = QtGui.QHBoxLayout(self)
         self.hboxlayout.addLayout(self.vboxlayout)
         self.hboxlayout.addWidget(self.sideeventmenu)
         #
-        self.widget.setLayout(self.hboxlayout);
-
+        self.widget.setLayout(self.hboxlayout)
 
         # self.widget.setLayout(self.vboxlayout)
 
         open = QtGui.QAction("&Open", self)
         self.connect(open, QtCore.SIGNAL("triggered()"), self.OpenFile)
-
-
 
         exit = QtGui.QAction("&Exit", self)
         self.connect(exit, QtCore.SIGNAL("triggered()"), sys.exit)
@@ -438,17 +429,18 @@ class Player(QtGui.QMainWindow):
         """Open a media file in a MediaPlayer
         """
         if filename is None:
-            filename = QtGui.QFileDialog.getOpenFileName(self, "Open File", os.path.expanduser('~'))
+            filename = QtGui.QFileDialog.getOpenFileName\
+                (self, "Open File", os.path.expanduser('~'))
         if not filename:
             return
-
 
     def OpenFile(self, filename=None):
         """Open a media file in a MediaPlayer
         """
         if filename is None:
             dir = os.getcwd()
-            filename = QtGui.QFileDialog.getOpenFileName(self, "Open File", os.path.expanduser(dir))
+            filename = QtGui.QFileDialog.getOpenFileName\
+                (self, "Open File", os.path.expanduser(dir))
         if not filename:
             return
 
@@ -471,11 +463,11 @@ class Player(QtGui.QMainWindow):
         # this is platform specific!
         # you have to give the id of the QFrame (or similar object) to
         # vlc, different platforms have different functions for this
-        if sys.platform.startswith('linux'): # for Linux using the X Server
+        if sys.platform.startswith('linux'):  # for Linux using the X Server
             self.mediaplayer.set_xwindow(self.videoframe.winId())
-        elif sys.platform == "win32": # for Windows
+        elif sys.platform == "win32":  # for Windows
             self.mediaplayer.set_hwnd(self.videoframe.winId())
-        elif sys.platform == "darwin": # for MacOS
+        elif sys.platform == "darwin":  # for MacOS
             self.mediaplayer.set_nsobject(self.videoframe.winId())
         self.PlayPause()
 
@@ -496,7 +488,7 @@ class Player(QtGui.QMainWindow):
         # setting the position to where the slider was dragged
         self.mediaplayer.set_position(position / 1000.0)
         # the vlc MediaPlayer needs a float value between 0 and 1, Qt
-        # uses integer variables, so you need a factor; the higher the
+        # uses integer variables, so you need a factor the higher the
         # factor, the more precise are the results
         # (1000 should be enough)
 
@@ -513,6 +505,7 @@ class Player(QtGui.QMainWindow):
                 # "Pause", not the desired behavior of a media player
                 # this will fix it
                 self.Stop()
+
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
